ec0ee2d1a47a8011dad2b8046924257301442395
julialonghi <julia.longhi@fecap.br>
2024-04-15 19:33:07 -0300

    update

--- atividades/atividade.js
+++ atividades/atividade.js
@@ -72,7 +72,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#E8EAED',
   },
   tasksWrapper: {
-    paddingTop: 80,
+    paddingTop: 15,
     paddingHorizontal: 20,
   },
   sectionTitle: {
